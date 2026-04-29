adcf88e176c51badfc1e278fbf0e5639af9d833a
Adisha Erangi <adishaerangikokilasanjeewani@gmail.com>
2026-04-29 19:44:12 +0530

    Finished work on About Us section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Methords from "./components/Home/Methords.jsx";
 import Hero from "./components/Home/Hero.jsx";
 import Packeges from "./components/Home/Packeges.jsx";
+import AboutUs from "./components/Home/AboutUs.jsx";
 function App() {
   return (
     <div>
       <Hero/>
       <Packeges/>
       < Methords/>
+      <AboutUs/>
 
     </div>
   );
