4cc448bdf3586f0a7fa48dfa335b336d056a340d
Mohammed musfir <mohammedmusfir81@gmail.com>
2026-04-29 18:46:33 +0530

    resolve the conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import Header from "./components/layout/Navbar.jsx";
 import Hero from "./components/Home/Hero.jsx";
-
-// App.tsx
-
-import React from "react";
-import Footer from "./components/Footer";
+import Footer from "./components/layout/Footer.jsx";
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
-    </div>
-  );
-    <div>
       <Footer />
     </div>
   );
+    
 }
 export default App;
 
 
 
-export default App;
