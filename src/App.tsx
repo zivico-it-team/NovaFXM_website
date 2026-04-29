9379eeb45515bebb9b73959dd4c391fa8c8cd042
Adisha Erangi <adishaerangikokilasanjeewani@gmail.com>
2026-04-29 13:09:55 +0530

    Finished work on adishaV2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Hero from "./components/Home/Hero.jsx";
+import Packeges from "./components/Home/Packeges.jsx";
 
 function App() {
   return (
     <div>
-      <Hero />
+      < Packeges/>
 
     </div>
   );
