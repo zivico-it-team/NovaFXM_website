21018fce2f05eb009157426a7770f6f9ad3ed4f6
Mohammed musfir <mohammedmusfir81@gmail.com>
2026-04-29 18:49:58 +0530

    add the Footer route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import Header from "./components/layout/Navbar.jsx";
 import Hero from "./components/Home/Hero.jsx";
 
-// App.tsx
-
-import React from "react";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
-    </div>
-  );
-    <div>
       <Footer />
     </div>
   );
+    
 }
 export default App;
 
 
 
-export default App;
