3eb97584fb4375a5677ab31439b6fa2d08e2d6fe
Mohammed musfir <mohammedmusfir81@gmail.com>
2026-04-30 11:40:40 +0530

    add the route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import Review from "./components/Home/Review.jsx";
 import Footer from "./components/layout/Footer.jsx";
+import Navbar from "./components/layout/Navbar.jsx";
+import Hero from "./components/Home/Hero.jsx";
+import AboutUs from "./components/Home/AboutUs.jsx";
+import Methords from "./components/Home/Methords.jsx";
+import Packeges from "./components/Home/Packeges.jsx";
 function App() {
   return (
     <div>
-       
+      <Navbar />
+      <Hero />
+      <AboutUs/>
+      <Methords/>
+      <Packeges/>
       <Review />
       <Footer /> 
     </div>
